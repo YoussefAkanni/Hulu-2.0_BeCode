8be8d5760185ed323de080ee4e4d0378b99c4ab0
Youssef Akanni <youssefakanni2003@gmail.com>
2021-12-27 16:27:47 +0100

    fetch to api /last modification

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -17,6 +19,26 @@ export default function Home() {
       {/*NavBar*/}
       <Nav />
       {/*Results*/}
+      <Results results={results} />
     </div>
   );
+}
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  try {
+    const request = await fetch(
+      `https://api.themoviedb.org/3${
+        requests[genre]?.url || requests.fetchTrending.url
+      }`
+    ).then((res) => res.json());
+
+    return {
+      props: {
+        results: request.results,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
 }
